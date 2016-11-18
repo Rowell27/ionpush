dd27c923717da8232871d5a7a54cf0ada793f537
Rowell27 <yahikodavid27@gmail.com>
2016-11-18 21:00:43 +0800

    Update send push

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,8 @@
 // <reference path = "../../../typings/globals/request/index.d.ts" />
 import { Component } from '@angular/core';
 import { Push } from '@ionic/cloud-angular';
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import { NavController, AlertController } from 'ionic-angular';
-import * as request from 'request';
-
-// let jsonpush = {
-//     "tokens": ["d1pEWtN2dHk:APA91bEoWUbrg_2xYEudBlEY-75hQ_1aKPHHLJXR56x4H9xpXq8HRBhqMiIJS3BwV1jLI4RsYYLqsVwT0oBUwa6r-tKy19AW0V2bSQ2w3MdbMgvqBQXAKtLR8PzaSq9C6CSMoYGRc4W6"],
-//     "profile": "ionpush2",
-//     "notification": {
-//         "message": "Manuel is awesome!"
-//     }
-// }
 
 @Component({
   selector: 'page-home',
@@ -19,10 +10,17 @@ import * as request from 'request';
 })
 export class HomePage {
 
+  url = "https://api.ionic.io"
   deviceToken = 'd1pEWtN2dHk:APA91bEoWUbrg_2xYEudBlEY-75hQ_1aKPHHLJXR56x4H9xpXq8HRBhqMiIJS3BwV1jLI4RsYYLqsVwT0oBUwa6r-tKy19AW0V2bSQ2w3MdbMgvqBQXAKtLR8PzaSq9C6CSMoYGRc4W6';
   token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0NDNlZjUxOC03NDg0LTQ2YmEtYWI0YS00OGVkZjBmNTRkMjUifQ.kHa-neGaPXRyf0_O9Gz5N22CtMaECH2mgyILRGOq0oY'
-  
-  text: string;
+  title: string;
+  message: string;
+
+  headers: Headers = new Headers({ 
+      "Content-Type" : "application/json", 
+      "Authorization" : "Bearer " + this.token  
+    });
+  options = new RequestOptions({ "headers" : this.headers })
 
   constructor(public navCtrl: NavController,
               public push: Push, 
@@ -40,59 +38,37 @@ export class HomePage {
         alert.present();
       });
 
-    // console.log(JSON.stringify(jsonpush))
+  }
 
+  getTokens(){
+
+    this.http.get( this.url + "/push/tokens" , this.options )
+        .subscribe((abc)=>{
+            console.log("Response: ",abc );
+        }, (e)=>{
+            console.log(e);
+        })
+    
   }
 
   onSend(){
 
-    // this.http.post( this.url, this.token , jsonpush ).subscribe( x =>{
-    //   console.log('err : ' + x );
-    // }
-
-    let options = {
-      method: 'POST',
-      url : 'https://api.ionic.io/push/notifications',
-      headers: {
-        'Authorization': 'Bearer ' + this.token,
-        'Content-Type': 'application/json'
-        }
-    };
-
-    let jsonpush = {
-        "tokens": this.deviceToken,
+    let data = {
+        // "tokens": [this.deviceToken],
+        "send_to_all" : true,
         "profile": "ionpush2",
         "notification": {
-            "message": "Hurray! Push Notification!"
-        } 
-    }
+          "title" : this.title,
+          "message": this.message
+          }
+      }
 
-    request( options, jsonpush, (err, response, body) => {
-      if (err) throw new Error(err);
-      console.log(JSON.stringify(jsonpush));
-    });
+      this.http.post( this.url + "/push/notifications", data , this.options)
+            .subscribe(()=>{
+              console.log("Push successfully send");
+            }, (e) =>{
+              console.log("Push failed to send. Error " + e);
+            });
+     }
 
-    // this.http.post( 'https://api.ionic.io/push/notifications', req, jsonpush ).subscribe( x=> {
-    //   console.log(x);
-    // });
-
-    
-    // let notification = {
-    //   "tokens": [this.deviceToken],
-    //   "profile": "ionpush2",
-    //   "notification": {
-    //     "title": "Hi",
-    //     "message": "Hello world!",
-    //     "android": {
-    //       "title": "Hey",
-    //       "message": "Hello Android!"
-    //     },
-    //     "ios": {
-    //       "title": "Howdy",
-    //       "message": "Hello iOS!"
-    //     } 
-    //   }
-    // };
-  
-  }
 }
